0b07ed2f4e5a94783fe993be3ab25273d854bc55
Qnbie <qnbie@pop-os.localdomain>
2024-02-12 10:55:37 +0100

    Add test character

--- GatewayGenesis/terrain_generator/terrain.gd
+++ GatewayGenesis/terrain_generator/terrain.gd
@@ -1,24 +1,29 @@
 extends MeshInstance3D
 
-
-var tmp = 0.5
+@onready var collison = $StaticBody3D/CollisionShape3D
 
 func _ready():
 	generate_new_terrain()
 
 func generate_new_terrain():
 	var fast_noise_lite = FastNoiseLite.new()
-	fast_noise_lite.seed = randi_range(1, 10)
+	
 	var texture = NoiseTexture2D.new()
 	texture.noise = fast_noise_lite
 	texture.normalize = true
-	texture.generate_mipmaps = true
 	
+	set_up_collision_shape(fast_noise_lite, 32, 32)
 	mesh.material.set_shader_parameter("noise", texture)
 
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_T:
-			print("T was pressed")
-			generate_new_terrain()
+func set_up_collision_shape(fnl: FastNoiseLite, depth: int, width: int):
+	var h_map_shape = HeightMapShape3D.new()
+	var packed_float_array = PackedFloat32Array()
+	for x in range(depth):
+		for y in range(width):
+			packed_float_array.append(fnl.get_noise_2d(x,y) * 5)
 
+	
+	h_map_shape.map_depth = depth
+	h_map_shape.map_width = width
+	h_map_shape.map_data = packed_float_array
+	collison.shape = h_map_shape
