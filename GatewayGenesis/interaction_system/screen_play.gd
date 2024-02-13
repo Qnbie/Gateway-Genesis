be40cacea6f59ebac0ecfbdf5410d7077d306a0b
Qnbie <qnbie@pop-os.localdomain>
2024-02-13 22:25:35 +0100

    Add json parser for screen_play

--- GatewayGenesis/interaction_system/screen_play.gd
+++ GatewayGenesis/interaction_system/screen_play.gd
@@ -2,4 +2,14 @@ extends Node
 
 class_name ScreenPlay
 
+var title: String
 var initial_line: String
+var content: Array
+
+func load_json(file_name: String):
+	var file = FileAccess.open("res://screen_play_library/" + file_name, FileAccess.READ)
+	var json = JSON.new()
+	var data = json.parse_string(file.get_as_text())
+	title = data.title
+	initial_line = data.initial_line
+	content = data.content
--- GatewayGenesis/npc_character/npc_character.gd
+++ GatewayGenesis/npc_character/npc_character.gd
@@ -6,7 +6,7 @@ var interaction_client: InteractionClient
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var screen_play = ScreenPlay.new()
-	screen_play.initial_line = "q > talk and e > craft"
+	screen_play.load_json("test.json")
 	player_character = get_parent_node_3d().get_player_character()
 	interaction_client = InteractionClient.new()
 	interaction_client.set_up(player_character, name, screen_play)
