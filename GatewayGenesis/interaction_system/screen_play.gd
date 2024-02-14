ab1cf82ef693b6e57c7aff706cb4730feb4cf184
Qnbie <qnbie@pop-os.localdomain>
2024-02-14 18:23:28 +0100

    Resolve file path bug

--- GatewayGenesis/interaction_system/screen_play.gd
+++ GatewayGenesis/interaction_system/screen_play.gd
@@ -7,7 +7,7 @@ var initial_line: String
 var content: Array
 
 func load_json(file_name: String):
-	var file = FileAccess.open("res://screen_play_library/" + file_name, FileAccess.READ)
+	var file = FileAccess.open("res://interaction_system/screen_play_library/" + file_name, FileAccess.READ)
 	var json = JSON.new()
 	var data = json.parse_string(file.get_as_text())
 	title = data.title
